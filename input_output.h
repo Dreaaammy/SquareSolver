0bb576e721c7364cb0e10d767bc504890df13507
Dreaaammy <117656478+Dreaaammy@users.noreply.github.com>
2023-08-29 14:41:31 +0300

    Update input_output.h
    adding many new features

--- input_output.h
+++ input_output.h
@@ -1,9 +1,17 @@
 #ifndef INPUT_OUTPUT_H
 #define INPUT_OUTPUT_H
 
-void PrintEquation              (const double a, const double b, const double c);
+typedef enum {
+            red = 12, green = 10,
+            blue = 9, white = 7
+            } Color;
+
+void ColorPrinting              (const Color color, const char text[]);
+void PrintEquation              (const Color color, const double a, const double b, const double c);
 void OutputAnswerText           (int nRoots, const double x1, const double x2);
-void InputCoeffs                (double* a, double* b, double* c);
+int  InputCoeffs                (double* a, double* b, double* c);
+void EnableColor                (const Color color);
+void DisableColor               ();
 void PrintHelloText             ();
 void FlushBuffer                ();
 void PrintRequestNumbersEnter   ();
