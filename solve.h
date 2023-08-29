56da04f7442cbb75d4b934dda458cdedf54d40a3
Dreaaammy <117656478+Dreaaammy@users.noreply.github.com>
2023-08-29 14:37:24 +0300

    Update solve.h
    addendum enum

--- solve.h
+++ solve.h
@@ -1,6 +1,11 @@
 #ifndef SOLVE_H
 #define SOLVE_H
 
+enum roots {
+            INF_ROOTS = -1, IMG_ROOTS = 3, ZERO_ROOTS = 0,
+            TWO_ROOTS = 2, ONE_ROOT = 1
+            };
+
 int SolveSquare         (const double a, const double b, const double c, double* x1, double* x2);
 int SolveLinear         (double b, double c, double* x1);
 
