6c292b88e969ebbc6e0ad41356cae374e96b942e
Dreaaammy <117656478+Dreaaammy@users.noreply.github.com>
2023-08-29 14:49:16 +0300

    Update equal_checking.h

--- equal_checking.h
+++ equal_checking.h
@@ -1,6 +1,8 @@
 #ifndef EQUAL_CHECKING_H
 #define EQUAL_CHECKING_H
 
+const double EPS = 1e-4;
+
 bool IsEqual        (const double num1, const double num2);
 bool IsZero         (const double num);
 
