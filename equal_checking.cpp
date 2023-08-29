41b087103f94018be5323b4330ec567dd1336d67
Dreaaammy <117656478+Dreaaammy@users.noreply.github.com>
2023-08-29 14:48:46 +0300

    Update equal_checking.cpp

--- equal_checking.cpp
+++ equal_checking.cpp
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "constants.h"
 #include "equal_checking.h"
 
+// Функция получает на вход два числа типа double,
+// проверяет их на равенство
 bool IsEqual(const double num1, const double num2)
 {
-    return fabs(num1 - num2) < EPS;
+    return fabs(num1 - num2) <= ((fabs(num1) > fabs(num2) ? fabs(num2) : fabs(num1)) * EPS);
 }
 
+// Функция сравнивает число с 0,
+// ипользуя функцию IsEqual()
 bool IsZero(const double num)
 {
     return IsEqual(num, 0);
